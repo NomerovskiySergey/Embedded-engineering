186710a7910a347134f7959f0c6b104c96d81087
Nomerovskyi Serhii <nomerushka@gmail.com>
2026-06-05 22:19:02 +0300

    dht11

---
+++ Nanit/sketch_dht11/sketch_dht11.ino
@@ -0,0 +1,19 @@
+#include <NanitLib.h>
+
+/*
+Translate to English:
+
+Write a program that uses a button and a temperature sensor. 
+When the button is pressed, the temperature and humidity values should be displayed in the Serial Monitor.
+*/
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
